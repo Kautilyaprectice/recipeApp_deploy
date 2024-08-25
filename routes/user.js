46ca8a2415e5139dfdcf5e8b4c10513f40468098
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-08-25 23:23:44 +0530

    routes managed

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userAuthentication = require('../middleware/authenticate');
 const router = express.Router();
 
 router.post('/user/signup', userController.createUser);
-router.post('/user/login', userController.loginUser);
+router.post('/', userController.loginUser);
 router.get('/user/profile', userAuthentication.authenticate, userController.getUserProfile);
 router.put('/user/profile', userAuthentication.authenticate, userController.updateUserProfile);
 router.get('/user/profile/contributed', userAuthentication.authenticate, userController.getContributedRecipes);
